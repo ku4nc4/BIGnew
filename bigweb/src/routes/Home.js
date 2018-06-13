83568b1779203009abee5e11a54062247b9d0fc4
Jason Kuanca <jhekhaa@gmail.com>
2018-06-13 15:02:18 +0700

    init commit

---
+++ bigweb/src/routes/Home.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="card">
+          <div className="card-header">
+            
+          </div>
+          <div className="card-body">
+            
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
